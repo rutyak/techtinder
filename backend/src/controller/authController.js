c34aa01fa01c7f37f46772f51570a86c2d956746
rutyak <rutikkhandekar12@gmail.com>
2025-11-12 19:07:05 +0530

    nodemailer-bug-fix

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -11,7 +11,9 @@ console.log("ENV check:", {
 });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
